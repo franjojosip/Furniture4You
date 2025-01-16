86142dba31f3bcb0110e3c8f5f2402e96be52511
franjojosip <franjojosip.jukic2@gmail.com>
2025-01-16 22:54:05 +0100

    - Add prelogin screen test

---
+++ app/src/androidTest/java/com/fjjukic/furniture4you/PreloginScreenTest.kt
@@ -0,0 +1,39 @@
+package com.fjjukic.furniture4you
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class PreloginScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun checkUiAndNavigate() {
+        val activity = composeTestRule.activity
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.title_prelogin).uppercase())
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.subtitle_prelogin).uppercase())
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.label_prelogin_description))
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.btn_get_started))
+            .performClick()
+    }
+}
